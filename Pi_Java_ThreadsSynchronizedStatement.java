8da85bb8a24e87d2ba6d7a75b8a8f2382f97a49e
Russel Winder <russel@russel.org.uk>
2010-04-19 16:14:49 +0100

    Fix the synchronization anomaly pointed out by Roger Orr.

--- Pi_Java_ThreadsSynchronizedStatement.java
+++ Pi_Java_ThreadsSynchronizedStatement.java
@@ -1,11 +1,16 @@
 /*
  *  Calculation of Pi using quadrature realized with a fork/join approach with threads.
  *
- *  Copyright © 2009 Russel Winder
+ *  Copyright © 2009-10 Russel Winder
  */
 
 public class Pi_Java_ThreadsSynchronizedStatement {
-  private static Double sum ; // Must be non-primitive! (The object lock of this object is used for synchronization.)
+  //  A singleton constant object used for all the synchronization.  As pointed out by Roger Orr (private
+  //  communication, 2010-04-19) there is a subtle bug in using the sum variable (if it were object) to synchronize
+  //  actions on itself since the variable refers to a different object after the update operation compared
+  //  to the one it refers to when the object lock is claimed.
+  private final static Integer lockObject = 1 ;
+  private static double sum ;
   private static void execute ( final int numberOfTasks ) {
     final long n = 1000000000l ;
     final double delta = 1.0 / n ;
@@ -24,14 +29,14 @@ public class Pi_Java_ThreadsSynchronizedStatement {
               final double x = ( i - 0.5 ) * delta ;
               localSum += 1.0 / ( 1.0 + x * x ) ;
             }
-            synchronized ( sum ) { sum += localSum ; }
+            synchronized ( lockObject ) { sum += localSum ; }
           }
         } ) ;
     }
     for ( final Thread t : threads ) { t.start ( ) ; }
     for ( final Thread t : threads ) {
       try { t.join ( ) ; }
-      catch ( final InterruptedException ie ) { throw new RuntimeException ( ie ) ; }
+      catch ( final InterruptedException ie ) { throw new RuntimeException ( "Got an InterruptedException joining a thread." , ie ) ; }
     }
     final double pi = 4.0 * sum * delta ;
     final double elapseTime = ( System.nanoTime ( ) - startTimeNanos ) / 1e9 ;
