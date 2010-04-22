9fdf59ec022ccd727cd4871c567b272849cc1d8f
Russel Winder <russel@russel.org.uk>
2010-04-22 21:01:17 +0100

    Refactor to use the short form declaration in the Go code.

--- pi_go_sequential.go
+++ pi_go_sequential.go
@@ -14,14 +14,14 @@ import (
 func main ( ) {
 	const n = int64 ( 1000000000 )
 	const delta = 1.0 / float64 ( n )
-	var startTime = time.Nanoseconds ( )
-	var sum = float64 ( 0.0 )
+	startTime := time.Nanoseconds ( )
+	sum := float64 ( 0.0 )
 	for  i := int64 ( 1 ) ; i <= n ; i++ {
-		var x = ( float64 ( i ) - 0.5 ) * delta
+		x := ( float64 ( i ) - 0.5 ) * delta
 		sum += 1.0 / ( 1.0 + x * x )
 	}
-	var pi = 4.0 * sum * delta
-	var elapseTime = float64 ( time.Nanoseconds ( ) - startTime ) / 1e9
+	pi := 4.0 * sum * delta
+	elapseTime := float64 ( time.Nanoseconds ( ) - startTime ) / 1e9
 	fmt.Printf ( "==== Go Sequential pi = %.18f\n" , pi ) ;
 	fmt.Printf ( "==== Go Sequential iteration count = %d\n" ,  n ) ;
 	fmt.Printf ( "==== Go Sequential elapse = %f\n" , elapseTime ) ;
