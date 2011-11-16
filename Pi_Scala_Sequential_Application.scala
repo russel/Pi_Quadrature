3b90d0d8aa8a4106d459cb544a8a2e7b164d9381
Russel Winder <russel@russel.org.uk>
2011-11-16 14:01:01 +0000

    Scala 2.9 App solves the problem with the previous Application class.

--- Pi_Scala_Sequential_Application.scala
+++ Pi_Scala_Sequential_Application.scala
@@ -4,12 +4,8 @@
  *  Copyright © 2009–2011 Russel Winder
  */
 
-//  When using the idiom of extending Application to create a main procedure, variables are represented as
-//  methods and this means performance seriously plummets.  The idiom must therefore be to only have
-//  function calls in the main sequence. cf. Pi_Scala_Sequential_Alt.scala.
-
 object Pi_Scala_Sequential_Application extends App {
-  val n = 1000000 // 1000 times fewer for performance reasons.
+  val n = 1000000000
   val delta = 1.0 / n
   val startTimeNanos = System.nanoTime
   var sum = 0.0
