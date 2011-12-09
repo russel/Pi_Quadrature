77287d0c509b62503ae7ead2b245ab2b43559a28
Russel Winder <russel@russel.org.uk>
2011-12-09 14:40:20 +0000

    Add a variant using dataflow variables in GPars.

---
+++ pi_groovy_gpars_dataflowVariables.groovy
@@ -0,0 +1,48 @@
+#! /usr/bin/env groovy
+
+/*
+ *  Caluclation of Pi using quadrature realized with GPars dataflow variables.
+ *
+ *  Copyright © 2011 Russel Winder
+ */
+
+@Grab ( 'org.codehaus.gpars:gpars:1.0-SNAPSHOT' )
+
+import groovyx.gpars.dataflow.DataflowVariable
+import static groovyx.gpars.dataflow.Dataflow.task
+
+void execute ( final int operatorCount ) {
+  final int n = 100000000i // 10 times fewer due to speed issues.
+  final double delta = 1.0d / n
+  final startTimeNanos = System.nanoTime ( )
+  final int sliceSize = n / operatorCount
+  final partialSums = [ ]
+  ( 0 ..< operatorCount ).each { index ->
+    partialSums << new DataflowVariable ( )
+    task {
+      final int start = 1i + index * sliceSize
+      final int end = ( index + 1i ) * sliceSize
+      double sum = 0.0d
+      for ( int i in start .. end ) {
+        final double x = ( i - 0.5d ) * delta
+        sum += 1.0d / ( 1.0d + x * x )
+      }
+      partialSums[index] << sum
+    }
+  }
+  final double pi = 4.0d * delta * partialSums.inject ( 0.0d ) { x , y -> x + y.val }
+  final elapseTime = ( System.nanoTime ( ) - startTimeNanos ) / 1e9
+  println ( '==== Groovy GPars DataflowVariables pi = ' + pi )
+  println ( '==== Groovy GPars DataflowVariables iteration count = ' + n )
+  println ( '==== Groovy GPars DataflowVariables elapse = ' + elapseTime )
+  println ( '==== Groovy GPars DataflowVariables processor count = ' + Runtime.runtime.availableProcessors ( ) ) ;
+  println ( '==== Groovy GPars DataflowVariables operator count = ' + operatorCount )
+}
+
+execute ( 1 )
+println ( )
+execute ( 2 )
+println ( )
+execute ( 8 )
+println ( )
+execute ( 32 )
