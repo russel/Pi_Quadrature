c6c2bd358a052838eefcfb09fbb5f40b5a6d2e3b
Russel Winder <russel@russel.org.uk>
2011-12-31 18:43:02 +0000

    NumPy has a fromfunction function.

---
+++ pi_python2_sequential_numpy_fromfunction_multiply.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- mode:python; coding:utf-8; -*-
+
+#  Calculation of Pi using quadrature. Sequential algorithm.  Use NumPy.
+#
+#  Copyright © 2008–2011 Russel Winder
+
+import time
+import numpy
+
+def f ( i ) :
+    x = ( i - 0.5 ) * delta
+    return 1.0 / ( 1.0 + x * x )
+
+if __name__ == '__main__' :
+    n = 100000000 # 0
+    delta = 1.0 / n
+    startTime = time.time ( )
+    pi = 4.0 * delta * numpy.fromfunction ( f , ( n , ) , dtype = numpy.float ).sum ( )
+    elapseTime = time.time ( ) - startTime
+    print ( "==== Python Sequential NumPy FromFunction Multiply pi = " + str ( pi ) )
+    print ( "==== Python Sequential NumPy FromFunction Multiply iteration count = " + str ( n ) )
+    print ( "==== Python Sequential NumPy FromFunction Multiply elapse = " + str ( elapseTime ) )
---
+++ pi_python2_sequential_numpy_fromfunction_power.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- mode:python; coding:utf-8; -*-
+
+#  Calculation of Pi using quadrature. Sequential algorithm.  Use NumPy.
+#
+#  Copyright © 2008–2011 Russel Winder
+
+import time
+import numpy
+
+if __name__ == '__main__' :
+    n = 100000000 # 0
+    delta = 1.0 / n
+    startTime = time.time ( )
+    pi = 4.0 * delta * numpy.fromfunction ( lambda i : 1.0 / ( 1.0 + ( ( i - 0.5 ) * delta ) ** 2 ) , ( n , ) , dtype = numpy.float ).sum ( )
+    elapseTime = time.time ( ) - startTime
+    print ( "==== Python Sequential NumPy FromFunction Power pi = " + str ( pi ) )
+    print ( "==== Python Sequential NumPy FromFunction Power iteration count = " + str ( n ) )
+    print ( "==== Python Sequential NumPy FromFunction Power elapse = " + str ( elapseTime ) )
