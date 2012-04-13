88c2f4a406facb30655873106b85b8643005efbb
Russel Winder <russel@russel.org.uk>
2012-04-13 20:32:29 +0100

    Use 64-bit rather than 80-bit floating point.

--- pi_fortran_mpi.f
+++ pi_fortran_mpi.f
@@ -1,13 +1,16 @@
 !  A Fortran program to calculate Pi using quadrature.  This is an SPMD realization using OpenMPI.
 !
-!  Copyright © 2008–2011 Russel Winder
+!  Copyright © 2008–2012 Russel Winder
 
 program pi
   use mpi
   implicit none
   integer , parameter :: n = 1000000000
-  double precision , parameter :: delta = 1.0 / n
-  double precision :: localSum = 0.0 , sum = 0.0 , elapseTime , pi_
+! For GFortran 6, 15, 18, 33 are known precisions.  It is assumed that 6 maps to 32-bit, 15 to 64-bit, and
+! 18 to 80-bit (if present). 33 presumably maps to a software 128-bit floating point library.
+  integer , parameter :: DoubleKind = selected_real_kind ( p = 15 )
+  real ( DoubleKind ) , parameter :: delta = 1.0 / n
+  real ( DoubleKind ) :: sum = 0.0 , elapseTime , pi_
   integer :: i , startTime , startFrequency , endTime , endFrequency
   integer :: errorState , nProcessors , myId , start , end , sliceSize
   call system_clock ( startTime , startFrequency )
