e4a06ce87dc4099ea73db82b73c98ab250cf50f1
Russel Winder <russel@russel.org.uk>
2012-04-14 19:14:04 +0100

    Make a correction to the comments and add an extra unusable variant for the future.

--- pi_d2_parallelReduce.d
+++ pi_d2_parallelReduce.d
@@ -4,9 +4,6 @@
  *  Copyright © 2011–2012 Russel Winder
  */
 
-//  std.parallelism is currently not in Phobos2, though it is being voted on for inclusion in Phobos2, so
-//  ensure the compilation command takes care of all the factors to include the library.
-
 //  This version is due to David Simcha, stemming from various emails on the various D email lists and
 //  reified in the documentation for std.parallelism,
 //  http://cis.jhu.edu/~dsimcha/d/phobos/std_parallelism.html#reduce
@@ -31,12 +28,11 @@ real getTerm ( int i ) {
 int main ( immutable string[] args ) {
   StopWatch stopWatch ;
   stopWatch.start ( ) ;
-  //
   //  There is a problem using a lambda function here.  David Simcha reports it is a consequence of issue
   //  5710 http://d.puremagic.com/issues/show_bug.cgi?id=5710.  Live with this and use the string syntax
   //  for specifying a lambda function.
-  //
   //immutable pi = 4.0 * delta * taskPool.reduce ! ( ( a , b ) { return a + b ; } ) ( map ! getTerm ( iota ( n ) ) ) ;
+  //immutable pi = 4.0 * delta * taskPool.reduce ! ( ( a , b ) => a + b ) ( map ! getTerm ( iota ( n ) ) ) ;
   immutable pi = 4.0 * delta * taskPool.reduce ! ( "a + b" ) ( map ! getTerm ( iota ( n ) ) ) ;
   stopWatch.stop ( ) ;
   immutable elapseTime = stopWatch.peek ( ).hnsecs * 100e-9 ;
