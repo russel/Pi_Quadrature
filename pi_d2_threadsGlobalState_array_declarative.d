0dd8cc946de3b2468feca21e6aaa1b9c476f7e47
Russel Winder <russel@russel.org.uk>
2012-04-15 17:00:46 +0100

    Remove the redundant material.

--- pi_d2_threadsGlobalState_array_declarative.d
+++ pi_d2_threadsGlobalState_array_declarative.d
@@ -11,10 +11,6 @@ import std.stdio ;
 
 import core.thread ;
 
-// A variable to select which of two thread setup sequences is to be used. 0 => use a map, anything else
-// means use a for loop.  There is a problem using a map in 2.059 :-((
-immutable selector = 1 ;
-
 shared double sum ;
 shared Object sumMutex ;
 
