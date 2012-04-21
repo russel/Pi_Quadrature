295976173ac351616c37c376368384bb4a03163f
Russel Winder <russel@russel.org.uk>
2012-04-21 17:57:38 +0100

    Formatting and some other changes.

--- pi_cpp_justThread_actors.cpp
+++ pi_cpp_justThread_actors.cpp
@@ -2,7 +2,7 @@
  *  A C++ program to calculate Pi using quadrature.  This uses Anthony Williams' Just::Threads Pro library which
  *  is an implementation of the threads specification of C++11 and has realizations of actors and dataflow.
  *
- *  Copyright © 2011 Russel Winder
+ *  Copyright © 2011–2012 Russel Winder
  */
 
 #include <iostream>
@@ -18,40 +18,32 @@ void execute ( const int numberOfWorkerActors ) {
   const auto delta = 1.0 / n ;
   const auto startTimeMicros = microsecondTime ( ) ;
   const auto sliceSize = n / numberOfWorkerActors ;
-  jss::actor accumulator (
-                          //  jss::actors are not copyable and so cannot use the [=] capture form, must use
-                          //  the [&] capture form.  Not a problem here as all the free variables in the
-                          //  lambda expression are const variables.
-                          [ & ] ( ) {
-                            auto sum = 0.0 ;
-                            for ( auto i = 0 ; i < numberOfWorkerActors ; ++i ) {
-                              jss::actor::receive ( )
-                               .match<double> (
-                                               [ & ] ( double d ) {
-                                                 sum += d ;
-                                               } ) ;
-                            }
-                            const auto pi = 4.0 * delta * sum ;
-                            const auto elapseTime = ( microsecondTime ( ) - startTimeMicros ) / 1e6 ;
-                            std::cout << "==== C++ Just::Thread actors pi = " << std::setprecision ( 18 ) << pi << std::endl ;
-                            std::cout << "==== C++ Just::Thread actors iteration count = " << n << std::endl ;
-                            std::cout << "==== C++ Just::Thread actors elapse = " << elapseTime << std::endl ;
-                            std::cout << "==== C++ Just::Thread actors threadCount = " <<  numberOfWorkerActors << std::endl ;
-                            std::cout << "==== C++ Just::Thread actors processor count = "  << std::thread::hardware_concurrency ( ) << std::endl ;
-                          } ) ;
+  jss::actor accumulator ( [ & ] ( ) {
+      auto sum = 0.0 ;
+      for ( auto i = 0 ; i < numberOfWorkerActors ; ++i ) {
+        jss::actor::receive ( )
+         .match<double> ( [ & ] ( double d ) { sum += d ; } ) ;
+      }
+      const auto pi = 4.0 * delta * sum ;
+      const auto elapseTime = ( microsecondTime ( ) - startTimeMicros ) / 1e6 ;
+      std::cout << "==== C++ Just::Thread actors pi = " << std::setprecision ( 18 ) << pi << std::endl ;
+      std::cout << "==== C++ Just::Thread actors iteration count = " << n << std::endl ;
+      std::cout << "==== C++ Just::Thread actors elapse = " << elapseTime << std::endl ;
+      std::cout << "==== C++ Just::Thread actors threadCount = " <<  numberOfWorkerActors << std::endl ;
+      std::cout << "==== C++ Just::Thread actors processor count = "  << std::thread::hardware_concurrency ( ) << std::endl ;
+    } ) ;
   std::unique_ptr<const jss::actor> calculators [ numberOfWorkerActors ] ;
   for ( auto index = 0 ; index < numberOfWorkerActors ; ++index ) {
-    calculators [ index ] = std::unique_ptr<const jss::actor> ( new jss::actor (
-                                                                                [ index , sliceSize , delta , &accumulator ] ( ) {
-                                                                                  const auto start = 1 + index * sliceSize ;
-                                                                                  const auto end = ( index + 1 ) * sliceSize ;
-                                                                                  auto sum = 0.0 ;
-                                                                                  for ( auto i = start ; i < end ; ++i ) {
-                                                                                    const auto x = ( i - 0.5 ) * delta ;
-                                                                                    sum += 1.0 / ( 1.0 + x * x ) ;
-                                                                                  }
-                                                                                  accumulator.send ( sum ) ;
-                                                                                } ) ) ;
+    calculators [ index ] = std::unique_ptr<const jss::actor> ( new jss::actor ( [ & ] ( ) {
+          const auto start = 1 + index * sliceSize ;
+          const auto end = ( index + 1 ) * sliceSize ;
+          auto sum = 0.0 ;
+          for ( auto i = start ; i < end ; ++i ) {
+            const auto x = ( i - 0.5 ) * delta ;
+            sum += 1.0 / ( 1.0 + x * x ) ;
+          }
+          accumulator.send ( sum ) ;
+        } ) ) ;
   }
 }
 
