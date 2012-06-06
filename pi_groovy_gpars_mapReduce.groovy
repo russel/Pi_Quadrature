f874ce104d897af6dc5a1de965afbdc984b48b1b
Russel Winder <russel@winder.org.uk>
2012-06-06 14:53:00 +0100

    Make the style consistent with other versions.

--- pi_groovy_gpars_mapReduce.groovy
+++ pi_groovy_gpars_mapReduce.groovy
@@ -1,6 +1,6 @@
 #! /usr/bin/env groovy
 
-import static groovyx.gpars.GParsPool.withPool
+import groovyx.gpars.GParsPool
 
 /*
  *  Calculation of Pi using quadrature realized with a map/reduce approach with GPars and hence harness all
@@ -9,13 +9,12 @@ import static groovyx.gpars.GParsPool.withPool
  *  Copyright © 2012 Russel Winder
  */
 void execute ( final int numberOfTasks ) {
-  final int n = 100000000i // 10 times fewer due to speed issues.
-  final double delta = 1.0d / n
-  final startTimeNanos = System.nanoTime ( )
-  final int sliceSize = n / numberOfTasks
-  final double pi
-  withPool {
-    pi = 4.0d * delta * ( 0i ..< numberOfTasks ).parallel.map { taskId ->
+  GParsPool.withPool {
+    final int n = 100000000i // 10 times fewer due to speed issues.
+    final double delta = 1.0d / n
+    final startTimeNanos = System.nanoTime ( )
+    final int sliceSize = n / numberOfTasks
+    final double pi = 4.0d * delta * ( 0i ..< numberOfTasks ).parallel.map { taskId ->
       final int start = 1i + taskId * sliceSize
       final int end = ( taskId + 1i ) * sliceSize
       double sum = 0.0d
@@ -25,13 +24,13 @@ void execute ( final int numberOfTasks ) {
       }
       sum
     }.sum ( )
+    final elapseTime = ( System.nanoTime ( ) - startTimeNanos ) / 1e9
+    println ( '==== Groovy GPars MapReduce pi = ' + pi )
+    println ( '==== Groovy GPars MapReduce iteration count = ' + n )
+    println ( '==== Groovy GPars MapReduce elapse = ' + elapseTime )
+    println ( '==== Groovy GPars MapReduce processor count = ' + Runtime.getRuntime ( ).availableProcessors ( ) )
+    println ( '==== Groovy GPars MapReduce task count = ' + numberOfTasks )
   }
-  final elapseTime = ( System.nanoTime ( ) - startTimeNanos ) / 1e9
-  println ( '==== Groovy GPars MapReduce pi = ' + pi )
-  println ( '==== Groovy GPars MapReduce iteration count = ' + n )
-  println ( '==== Groovy GPars MapReduce elapse = ' + elapseTime )
-  println ( '==== Groovy GPars MapReduce processor count = ' + Runtime.getRuntime ( ).availableProcessors ( ) )
-  println ( '==== Groovy GPars MapReduce task count = ' + numberOfTasks )
 }
 
 execute ( 1 )
