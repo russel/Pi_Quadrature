14bba6d89cdf5ff278bd93a04fbf4615c787dbf8
Russel Winder <russel@winder.org.uk>
2012-06-18 12:57:20 +0100

    Lower case rather than upper case π for the output. Extend the parameter handling to allow more information.

--- output.py
+++ output.py
@@ -8,9 +8,12 @@ A module providing an output function for all the variants of the "Π by Quadrat
 
 def out ( banner , pi , n , elapseTime , *args ) :
     print ( '================ {}'.format ( banner ) )
-    print ( '\tΠ = {:.18f}'.format ( pi ) )
+    print ( '\tπ = {:.18f}'.format ( pi ) )
     print ( '\titeration count = {}'.format ( n ) )
     print ( '\telapse time = {}'.format ( elapseTime ) )
     if len ( args ) > 0 :
-        assert len ( args ) == 1
-        print ( '\ttask count = {}'.format ( args[0] ) ) 
+        print ( '\ttask count = {}'.format ( args[0] ) )
+        if len ( args ) > 1 :
+            print ( '\tprocessor count = {}'.format ( args[1] ) )
+        else :
+            raise ValueError ( 'Too many parameters to out function.' )
