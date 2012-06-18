83830c46cc8754a4d77c3220381cc6ead8c1fe01
Russel Winder <russel@winder.org.uk>
2012-06-18 08:54:33 +0100

    Update the MPI4Py versions.

--- pi_python2_mpi4py.py
+++ pi_python2_mpi4py.py
@@ -1,33 +1,32 @@
 #! /usr/bin/env python
 # -*- mode:python; coding:utf-8; -*-
 
-#  A Python program to calculate Pi using quadrature.  This is an SPMD realization using OpenMPI under the
+#  A Python program to calculate Pi using quadrature. This is an SPMD realization using OpenMPI under the
 #  mpi4py package that provides Python binding to MPI.
 #
-#  Copyright © 2010–2011 Russel Winder
+#  Copyright © 2010–2012 Russel Winder
 
-import time
 from mpi4py import MPI
-import numpy
+from numpy import array
+from output import out
+from time import time
+
 
 if __name__ == '__main__' :
-  n = 10000000 # 100 times fewer due to speed issues.
+  n = 10000000 # 100 times fewer than C due to speed issues.
   delta = 1.0 / n
-  startTime = time.time ( )
+  startTime = time ( )
   comm = MPI.COMM_WORLD
   nProcessors = comm.Get_size ( )
   myId = comm.Get_rank ( )
   sliceSize = n / nProcessors
-  localSum = numpy.array ( [ 0.0 ] )
+  localSum = array ( [ 0.0 ] )
   for i in xrange ( 1 + myId * sliceSize , ( myId + 1 ) * sliceSize ) :
     x = ( i - 0.5 ) * delta
     localSum[0] += 1.0 / ( 1.0 + x * x )
-  sum = numpy.array ( [ 0.0 ] )
+  sum = array ( [ 0.0 ] )
   comm.Reduce ( ( localSum , MPI.DOUBLE ) , ( sum , MPI.DOUBLE ) )
   if myId == 0 :
     pi = 4.0 * delta * sum[0]
-    elapseTime = time.time ( ) - startTime
-    print ( "==== Python MPI pi = " + str ( pi ) )
-    print ( "==== Python MPI iteration count = " + str ( n ) )
-    print ( "==== Python MPI elapse = " + str ( elapseTime ) )
-    print ( "==== Python MPI processorCount = " + str ( nProcessors ) )
+    elapseTime = time ( ) - startTime
+    out ( 'Python2 MPI4Py' , pi , n , elapseTime , nProcessors )
--- pi_python2_mpi4py_extension_c.py
+++ pi_python2_mpi4py_extension_c.py
@@ -1,35 +1,33 @@
 #! /usr/bin/env python
 # -*- mode:python; coding:utf-8; -*-
 
-#  A Python program to calculate Pi using quadrature.  This is an SPMD realization using OpenMPI under the
+#  A Python program to calculate Pi using quadrature. This is an SPMD realization using OpenMPI under the
 #  mpi4py package that provides Python binding to MPI.
 #
 #  Copyright © 2010–2012 Russel Winder
 
-import time
 from mpi4py import MPI
-import numpy
+from numpy import array
+from output import out
+from time import time
+
 import ctypes
 
 if __name__ == '__main__' :
   processSliceModule = ctypes.cdll.LoadLibrary ( 'processSlice_c.so' )
   processSliceModule.processSlice.argtypes = [ ctypes.c_int , ctypes.c_int , ctypes.c_double ]
   processSliceModule.processSlice.restype = ctypes.c_double
-
   n = 1000000000
   delta = 1.0 / n
-  startTime = time.time ( )
+  startTime = time ( )
   comm = MPI.COMM_WORLD
   nProcessors = comm.Get_size ( )
   myId = comm.Get_rank ( )
   sliceSize = n / nProcessors
-  localSum = numpy.array ( [ processSliceModule.processSlice ( myId , sliceSize , delta ) ] )
-  sum = numpy.array ( [ 0.0 ] )
+  localSum = array ( [ processSliceModule.processSlice ( myId , sliceSize , delta ) ] )
+  sum = array ( [ 0.0 ] )
   comm.Reduce ( ( localSum , MPI.DOUBLE ) , ( sum , MPI.DOUBLE ) )
   if myId == 0 :
     pi = 4.0 * delta * sum[0]
-    elapseTime = time.time ( ) - startTime
-    print ( "==== Python MPI C Extension pi = " + str ( pi ) )
-    print ( "==== Python MPI C Extension iteration count = " + str ( n ) )
-    print ( "==== Python MPI C Extension elapse = " + str ( elapseTime ) )
-    print ( "==== Python MPI C Extension processorCount = " + str ( nProcessors ) )
+    elapseTime = time ( ) - startTime
+    out ( 'Python2 MPI4Py Extension C' , pi , n , elapseTime , nProcessors )
