30d096aeb950720c7a42269ad01acd9511b90a5c
Russel Winder <russel@winder.org.uk>
2012-08-03 19:53:14 +0100

    Add a D output module.

---
+++ output_d.d
@@ -0,0 +1,19 @@
+/*
+ * Output functions for the D realizations of π by quadrature.
+ *
+ * Copyright © 2012 Russel Winder
+ */
+
+import std.conv ;
+import std.stdio ;
+
+void output ( string banner , double pi , int n , double elapseTime ) {
+  writeln ( "======================== " ~ banner ) ;
+  writefln ( "\tπ = %.18f" , pi ) ;
+  writefln ( "\titeration count = %d" , n ) ;
+  writefln ( "\telapse time = %f" , elapseTime ) ;
+}
+
+void output ( string banner , double pi , int n , double elapseTime , int numberOfTasks ) {
+  output ( banner ~ ", number of tasks: " ~ to!string ( numberOfTasks ) , pi , n , elapseTime ) ;
+}
