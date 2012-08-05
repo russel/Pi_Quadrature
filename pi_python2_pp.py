39d46cd05e8ef51e7315a766b74b67e228b922a0
Russel Winder <russel@winder.org.uk>
2012-08-05 20:38:35 +0100

    Various changes to comments and a reformatting to PEP-8.

--- pi_python2_pp.py
+++ pi_python2_pp.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python
-# -*- mode:python; coding:utf-8; -*-
+# -*- coding:utf-8; -*-
 
-#  Calculation of Pi using quadrature. Use the Parallel Python package. Parallel Python appears not to be
-#  packaged for use with Python 3 in the Ubuntu package repository.
+#  Calculation of π using quadrature. Use the Parallel Python package.
 #
 #  Copyright © 2008–2012 Russel Winder
 
@@ -10,27 +9,27 @@ from output import out
 from pp import Server
 from time import time
 
-def processSlice ( id , sliceSize , delta ) :
+def processSlice(id, sliceSize, delta):
     sum = 0.0
-    for i in xrange ( 1 + id * sliceSize , ( id + 1 ) * sliceSize + 1 ) :
-        x = ( i - 0.5 ) * delta
-        sum += 1.0 / ( 1.0 + x * x )
+    for i in xrange(1 + id * sliceSize, (id + 1) * sliceSize + 1):
+        x = (i - 0.5) * delta
+        sum += 1.0 / (1.0 + x * x)
     return sum
 
-def execute ( processCount ) :
-    n = 10000000 # 100 times fewer than C due to speed issues.
+def execute(processCount):
+    n = 10000000  # 100 times fewer than C due to speed issues.
     delta = 1.0 / n
-    startTime = time ( )
+    startTime = time()
     sliceSize = n / processCount
-    server = Server ( secret = 'blahblahblah' )
-    jobs = [ server.submit ( processSlice , ( i , sliceSize , delta ) ) for i in xrange ( 0, processCount ) ]
-    pi = 4.0 * delta * sum ( [ job ( ) for job in jobs ] )
-    elapseTime = time ( ) - startTime
-    out ( __file__ , pi , n , elapseTime , processCount )
-    server.print_stats ( )
-    
-if __name__ == '__main__' :
-    execute ( 1 )
-    execute ( 2 )
-    execute ( 8 )
-    execute ( 32 )
+    server = Server(secret='blahblahblah')
+    jobs = [server.submit(processSlice, (i, sliceSize, delta)) for i in xrange(0, processCount)]
+    pi = 4.0 * delta * sum([job() for job in jobs])
+    elapseTime = time() - startTime
+    out(__file__, pi, n, elapseTime, processCount)
+    server.print_stats()
+
+if __name__ == '__main__':
+    execute(1)
+    execute(2)
+    execute(8)
+    execute(32)
