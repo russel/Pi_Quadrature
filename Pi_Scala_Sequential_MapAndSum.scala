0b77e5dc17f601d0b650160cc9c4476c9942de6d
Russel Winder <russel@winder.org.uk>
2012-10-11 12:01:54 +0100

    Remove any doubt about performance as an App.

--- Pi_Scala_Sequential_MapAndSum.scala
+++ Pi_Scala_Sequential_MapAndSum.scala
@@ -6,17 +6,17 @@
 
 import SOutput.out
 
-//  This works but is horrendously slow.
-
-object Pi_Scala_Sequential_MapAndSum extends App {
-  val n = 1000000000
-  val delta = 1.0 / n
-  val startTimeNanos = System.nanoTime
-  def f(i:Int):Double = {
-    val x = (i - 0.5) * delta
-    1.0 / (1.0 + x * x)
-  }
-  val pi = 4.0 * delta * (0 until n).iterator.map(f).sum
-  val elapseTime = (System.nanoTime - startTimeNanos) / 1e9
-  out("Pi_Scala_Futures", pi, n, elapseTime)
+object Pi_Scala_Sequential_MapAndSum {
+ def main(args:Array[String]) {
+   val n = 1000000000
+   val delta = 1.0 / n
+   val startTimeNanos = System.nanoTime
+   def f(i:Int):Double = {
+     val x = (i - 0.5) * delta
+     1.0 / (1.0 + x * x)
+   }
+   val pi = 4.0 * delta * (0 until n).iterator.map(f).sum
+   val elapseTime = (System.nanoTime - startTimeNanos) / 1e9
+   out("Pi_Scala_Futures", pi, n, elapseTime)
+ }
 }
