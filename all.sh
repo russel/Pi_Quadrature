af21620ed415fae2cf86096cddc1238db867f836
Russel Winder <russel@winder.org.uk>
2013-01-05 16:36:23 +0000

    Add Java build to the all.sh script.

--- all.sh
+++ all.sh
@@ -1,7 +1,19 @@
 #! /bin/sh
 
+banner() {
+    echo "========================  $1  ===="
+}
+
 for d in ` ls */SConstruct | sed 's/\/SConstruct//g'`
 do
-    echo "====================  $d  ===="
-    (cd $d; python /home/users/russel/Repositories/Mercurial/Masters/SCons_D_Tooling/bootstrap.py $*)
+    if [ $# -gt 0 -a $1 = clean ]; then
+        parameter=-c
+    else
+        parameter="$*"
+    fi
+    banner $d
+    (cd $d; python /home/users/russel/Repositories/Mercurial/Masters/SCons_D_Tooling/bootstrap.py $parameter)
 done
+
+banner Java
+(cd Java; gradlew $*)
