9f8ee9bb3a2befdd6dfb7dca201e1ee444a49128
Russel Winder <russel@winder.org.uk>
2013-03-22 20:55:18 +0000

    First cut at a Java 8 streams version. Compiles with b82.

---
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Pi_Streams.java
@@ -0,0 +1,38 @@
+/*
+ *  Calculation of π using quadrature realized with an approached based on Java 8 streams.
+ *
+ *  Copyright © 2013  Russel Winder
+ */
+
+package uk.org.winder.pi_quadrature;
+
+import java.util.Arrays;
+
+public class Pi_Streams {
+  private static void execute(final int numberOfTasks) {
+    final int n = 1000000000;
+    final double delta = 1.0 / n;
+    final long startTimeNanos = System.nanoTime();
+    final int sliceSize = n / numberOfTasks;
+    final int[] indexes = new int[numberOfTasks];
+    for (int i = 0 ; i < indexes.length; ++i) { indexes[i] = i; }
+    final double pi = 4.0 * delta * Arrays.parallelStream(indexes).map((final int taskId) -> {
+        final int start = 1 + taskId * sliceSize;
+        final int end = (taskId + 1) * sliceSize;
+        double sum = 0.0;
+        for (int i = start; i <= end; ++i) {
+          final double x = (i - 0.5) * delta;
+          sum += 1.0 / (1.0 + x * x);
+        }
+        return sum;
+      }).reduce((double a, double b) -> a + b).getAsDouble();
+    final double elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
+    Output.out("Pi_Streams", pi, n, elapseTime, numberOfTasks);
+  }
+  public static void main(final String[] args) {
+    Pi_Streams.execute(1);
+    Pi_Streams.execute(2);
+    Pi_Streams.execute(8);
+    Pi_Streams.execute(32);
+  }
+}
