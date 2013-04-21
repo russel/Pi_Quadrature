97a5e7c123306af6abf4a6e690ccb5337ec3ca8b
Russel Winder <russel@winder.org.uk>
2013-04-21 16:54:36 +0100

    Correct the formatting and things.

--- Python/processSlice_c.c
+++ Python/processSlice_c.c
@@ -1,16 +1,16 @@
 /*
  *  A C function to calculate a slice of the overall calculation of Pi using quadrature.
  *
- *  Copyright © 2009–2011 Russel Winder
+ *  Copyright © 2009–2011, 2013  Russel Winder
  */
 
-double processSlice ( int id , int sliceSize , double delta ) {
-  const int start = 1 + id * sliceSize ;
-  const int end = ( id + 1 ) * sliceSize ;
-  double sum = 0.0 ;
-  for ( int i = start ; i <= end ; ++i ) {
-    const double x = ( i - 0.5 ) * delta ;
-    sum += 1.0 / ( 1.0 + x * x ) ;
+double processSlice(int id, int sliceSize, double delta) {
+  const int start = 1 + id * sliceSize;
+  const int end = (id + 1) * sliceSize;
+  double sum = 0.0;
+  for (int i = start; i <= end; ++i) {
+    const double x = (i - 0.5) * delta;
+    sum += 1.0 / (1.0 + x * x);
   }
-  return sum ;
+  return sum;
 }
--- Python/processSlice_cpp.cpp
+++ Python/processSlice_cpp.cpp
@@ -1,18 +1,17 @@
 /*
  *  A C++ function to calculate a slice of the overall calculation of Pi using quadrature.
  *
- *  Copyright © 2009–2011 Russel Winder
+ *  Copyright © 2009–2011, 2013  Russel Winder
  */
 
-extern "C" double processSlice ( int id , int sliceSize , double delta ) ;
-
-double processSlice ( const int id , const int sliceSize , const double delta ) {
-  const auto start = 1 + id * sliceSize ;
-  const auto end = ( id + 1 ) * sliceSize ;
-  auto sum = 0.0 ;
-  for ( auto i = start ; i <= end ; ++i ) {
-    const auto x = ( i - 0.5 ) * delta ;
-    sum += 1.0 / ( 1.0 + x * x ) ;
+extern "C"
+double processSlice(const int id, const int sliceSize, const double delta) {
+  const auto start = 1 + id * sliceSize;
+  const auto end = (id + 1) * sliceSize;
+  auto sum = 0.0;
+  for (auto i = start; i <= end; ++i) {
+    const auto x = (i - 0.5) * delta;
+    sum += 1.0 / (1.0 + x * x);
   }
-  return sum ;
+  return sum;
 }
