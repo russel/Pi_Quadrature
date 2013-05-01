a44c33b777edf0722134a1e4c304419cdb001221
Russel Winder <russel@winder.org.uk>
2013-05-01 08:15:52 +0100

    Reformat the Cython source. Recognize that the type of x can be inferred correctly.

--- Python/processSlice_cython_py2.pyx
+++ Python/processSlice_cython_py2.pyx
@@ -2,13 +2,12 @@
 
 #  A Cython extensions to calculate a slice of the overall calculation of π using quadrature.
 #
-#  Copyright © 2010–2011 Russel Winder
+#  Copyright © 2010–2013 Russel Winder
 
-def processSlice ( int id , int sliceSize , double delta ) :
+def processSlice(int id, int sliceSize, double delta):
   cdef int i = 0
   cdef double sum = 0.0
-  cdef double x
-  for i in xrange ( 1 + id * sliceSize , ( id + 1 ) * sliceSize ) :
-    x = ( i - 0.5 ) * delta
-    sum += 1.0 / ( 1.0 + x * x )
+  for i in xrange(1 + id * sliceSize, (id + 1) * sliceSize):
+    x = (i - 0.5) * delta
+    sum += 1.0 / (1.0 + x * x)
   return sum
--- Python/processSlice_cython_py3.pyx
+++ Python/processSlice_cython_py3.pyx
@@ -1,12 +1,11 @@
 #  A Cython extensions to calculate a slice of the overall calculation of π using quadrature.
 #
-#  Copyright © 2010–2012 Russel Winder
+#  Copyright © 2010–2013 Russel Winder
 
-def processSlice ( int id , int sliceSize , double delta ) :
+def processSlice(int id, int sliceSize, double delta):
   cdef int i = 0
   cdef double sum = 0.0
-  cdef double x
-  for i in range ( 1 + id * sliceSize , ( id + 1 ) * sliceSize ) :
-    x = ( i - 0.5 ) * delta
-    sum += 1.0 / ( 1.0 + x * x )
+  for i in range(1 + id * sliceSize, (id + 1) * sliceSize):
+    x = (i - 0.5) * delta
+    sum += 1.0 / (1.0 + x * x)
   return sum
