d7c73933a2ec33ed3c1515443f80f22343ef619a
Russel Winder <russel@winder.org.uk>
2013-05-01 08:15:20 +0100

    Reformat the Pyrex source.

--- Python/processSlice_pyrex_py2.pyx
+++ Python/processSlice_pyrex_py2.pyx
@@ -2,13 +2,13 @@
 
 #  A Pyrex extensions to calculate a slice of the overall calculation of π using quadrature.
 #
-#  Copyright © 2010–2011 Russel Winder
+#  Copyright © 2010–2013 Russel Winder
 
-def processSlice ( int id , int sliceSize , double delta ) :
+def processSlice(int id, int sliceSize, double delta):
   cdef int i
-  cdef double sum , x
+  cdef double sum, x
   sum = 0.0
-  for i in xrange ( 1 + id * sliceSize , ( id + 1 ) * sliceSize + 1 ) :
-    x = ( i - 0.5 ) * delta
-    sum += 1.0 / ( 1.0 + x * x )
+  for i in xrange(1 + id * sliceSize, (id + 1) * sliceSize + 1):
+    x = (i - 0.5) * delta
+    sum += 1.0 / (1.0 + x * x)
   return sum
