2b72cff012bd95e33e8de881f4726a3d09f07ce5
Russel Winder <russel@winder.org.uk>
2013-05-06 20:23:03 +0100

    Remove all the parallel bits from what is supposed to be sequential.

--- Java/src/main/java/uk/org/winder/pi_quadrature/Pi_SequentialStream_Java8.java
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Pi_SequentialStream_Java8.java
@@ -9,28 +9,16 @@ package uk.org.winder.pi_quadrature;
 import java.util.stream.IntStream;
 
 public class Pi_SequentialStream_Java8 {
-  private static void execute(final int numberOfTasks) {
+  public static void main(final String[] args) {
     final int n = 1000000000;
     final double delta = 1.0 / n;
     final long startTimeNanos = System.nanoTime();
     final int sliceSize = n / numberOfTasks;
-    final double pi = 4.0 * delta * IntStream.range(0, numberOfTasks).mapToDouble(taskId -> {
-        final int start = 1 + taskId * sliceSize;
-        final int end = (taskId + 1) * sliceSize;
-        double sum = 0.0;
-        for (int i = start; i <= end; ++i) {
-          final double x = (i - 0.5) * delta;
-          sum += 1.0 / (1.0 + x * x);
-        }
-        return sum;
+    final double pi = 4.0 * delta * IntStream.range(0, n).mapToDouble(taskId -> {
+        final double x = (i - 0.5) * delta;
+        return 1.0 / (1.0 + x * x);
       }).sum();
     final double elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
     Output.out("Pi_SequentialStream_Java8", pi, n, elapseTime, numberOfTasks);
-  }
-  public static void main(final String[] args) {
-    Pi_SequentialStream_Java8.execute(1);
-    Pi_SequentialStream_Java8.execute(2);
-    Pi_SequentialStream_Java8.execute(8);
-    Pi_SequentialStream_Java8.execute(32);
   }
 }
