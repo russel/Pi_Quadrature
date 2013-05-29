8b9260fece0b5e8e7cb156ad220099f3440bf96d
Russel Winder <russel@winder.org.uk>
2013-05-29 10:32:46 +0100

    Make Julia sequential code 40 times faster by putting the code in a function and calling it.

--- Julia/pi_sequential.jl
+++ Julia/pi_sequential.jl
@@ -6,13 +6,17 @@
 
 require("output.jl")
 
-n = 10000000  # 100 times fewer than C due to speed issues.
-delta = 1.0 / n
-startTime = time()
-sum = 0.0
-for i = 1:(n + 1)
-    sum += 1.0 / (1.0 + ((i - 0.5) * delta) ^ 2)
+function execute()
+    n = 1000000000
+    delta = 1.0 / n
+    startTime = time()
+    sum = 0.0
+    for i = 1:(n + 1)
+        sum += 1.0 / (1.0 + ((i - 0.5) * delta) ^ 2)
+    end
+    pi = 4.0 * delta * sum
+    elapseTime = time() - startTime
+    out("pi_julia_sequential", pi, n, elapseTime)
 end
-pi = 4.0 * delta * sum
-elapseTime = time() - startTime
-out("pi_julia_sequential", pi, n, elapseTime)
+
+execute()
