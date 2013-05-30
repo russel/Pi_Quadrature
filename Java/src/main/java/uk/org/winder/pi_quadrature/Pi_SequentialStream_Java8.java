d52886f8f2d46eb49a2950e2920e2d519f5fdc29
Russel Winder <russel@winder.org.uk>
2013-05-30 17:43:34 +0100

    Correct the Java 8 streams sequential code.

--- Java/src/main/java/uk/org/winder/pi_quadrature/Pi_SequentialStream_Java8.java
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Pi_SequentialStream_Java8.java
@@ -13,12 +13,11 @@ public class Pi_SequentialStream_Java8 {
     final int n = 1000000000;
     final double delta = 1.0 / n;
     final long startTimeNanos = System.nanoTime();
-    final int sliceSize = n / numberOfTasks;
-    final double pi = 4.0 * delta * IntStream.range(0, n).mapToDouble(taskId -> {
+    final double pi = 4.0 * delta * IntStream.range(0, n).mapToDouble(i -> {
         final double x = (i - 0.5) * delta;
         return 1.0 / (1.0 + x * x);
       }).sum();
     final double elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
-    Output.out("Pi_SequentialStream_Java8", pi, n, elapseTime, numberOfTasks);
+    Output.out("Pi_SequentialStream_Java8", pi, n, elapseTime);
   }
 }
