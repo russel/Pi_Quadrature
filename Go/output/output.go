55c9ad4dddf9d52ab719c0874620a71ceb8b3488
Russel Winder <russel@winder.org.uk>
2013-08-04 20:02:53 +0100

    Amend the Go output functions a bit.

--- Go/output/output.go
+++ Go/output/output.go
@@ -6,20 +6,18 @@ package output
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func OutS(banner string, pi float64, n int32, elapseTime time.Duration) {
-	fmt.Printf("================ %s\n", banner)
+	fmt.Printf("======================== %s\n", banner)
 	fmt.Printf("\tπ = %.18f\n", pi)
 	fmt.Printf("\titeration count = %d\n", n)
 	fmt.Printf("\telapse time = %v\n", elapseTime)
 }
 
 func OutP(banner string, pi float64, n int32, elapseTime time.Duration, numberOfTasks int) {
-	fmt.Printf("================ %s\n", banner)
-	fmt.Printf("\tπ = %.18f\n", pi)
-	fmt.Printf("\titeration count = %d\n", n)
-	fmt.Printf("\telapse time = %v\n", elapseTime)
-	fmt.Printf("\tnumber of tasks = %d\n", numberOfTasks)
+	OutS(banner+fmt.Sprintf(", number of tasks: %d", numberOfTasks), pi, n, elapseTime)
+	fmt.Printf("\tnumber of processors = %d\n", runtime.NumCPU())
 }
