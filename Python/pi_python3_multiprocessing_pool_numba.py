33de9f36cec3741078427a08a3797d8556ab9618
Russel Winder <russel@winder.org.uk>
2013-08-28 18:30:55 +0100

    Add some variants using the Numba package to JIT the Python loop.

---
+++ Python/pi_python3_multiprocessing_pool_numba.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+#  Calculation of π using quadrature. Uses the multiprocessing package to provide a process pool to enable
+#  asynchronous function calls very akin to futures.
+#
+#  Copyright © 2008–2013 Russel Winder
+
+from multiprocessing import Pool
+from output import out
+from time import time
+
+from numba import autojit
+
+@autojit
+def processSlice(id, sliceSize, delta):
+    sum = 0.0
+    for i in range(1 + id * sliceSize, (id + 1) * sliceSize + 1):
+        x = (i - 0.5) * delta
+        sum += 1.0 / (1.0 + x * x)
+    return sum
+
+def execute(processCount):
+    n = 10000000  # 100 times fewer than C due to speed issues.
+    delta = 1.0 / n
+    startTime = time()
+    sliceSize = n // processCount
+    pool = Pool(processes=processCount)
+    results = [pool.apply_async(processSlice, args=(i, sliceSize, delta)) for i in range(0, processCount)]
+    pi = 4.0 * delta * sum(item.get() for item in results)
+    elapseTime = time() - startTime
+    out(__file__, pi, n, elapseTime, processCount)
+
+if __name__ == '__main__':
+    execute(1)
+    execute(2)
+    execute(8)
+    execute(32)
---
+++ Python/pi_python3_multiprocessing_process_numba.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+#  Calculation of π using quadrature. Uses the multiprocessing package with processes sending messages to
+#  a collecting queue.
+#
+#  Copyright © 2008–2013 Russel Winder
+
+from multiprocessing import Queue, Process
+from output import out
+from time import time
+
+from numba import autojit
+
+@autojit
+def processSlice(id, sliceSize, delta, output):
+    sum = 0.0
+    for i in range(1 + id * sliceSize, (id + 1) * sliceSize + 1):
+        x = (i - 0.5) * delta
+        sum += 1.0 / (1.0 + x * x)
+    output.put(sum)
+
+def execute(processCount):
+    n = 10000000  # 100 times fewer than C due to speed issues.
+    delta = 1.0 / n
+    startTime = time()
+    sliceSize = n // processCount
+    resultsQueue = Queue()
+    processes = [Process(target=processSlice, args=(i, sliceSize, delta, resultsQueue)) for i in range(0, processCount)]
+    for p in processes:
+        p.start()
+    pi = 4.0 * delta * sum(resultsQueue.get() for i in range(0, processCount))
+    elapseTime = time() - startTime
+    out(__file__, pi, n, elapseTime, processCount)
+
+if __name__ == '__main__':
+    execute(1)
+    execute(2)
+    execute(8)
+    execute(32)
