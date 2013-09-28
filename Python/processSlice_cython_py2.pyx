fac930ed2006dff1119f4162120448f2b4b1c632
Russel Winder <russel@winder.org.uk>
2013-09-28 17:20:27 +0100

    Release the GIL in the Cython codes.

--- Python/processSlice_cython_py2.pyx
+++ Python/processSlice_cython_py2.pyx
@@ -5,9 +5,10 @@
 #  Copyright © 2010–2013 Russel Winder
 
 def processSlice(int id, int sliceSize, double delta):
-  cdef int i = 0
-  cdef double sum = 0.0
-  for i in xrange(1 + id * sliceSize, (id + 1) * sliceSize):
-    x = (i - 0.5) * delta
-    sum += 1.0 / (1.0 + x * x)
-  return sum
+    cdef int i = 0
+    cdef double sum = 0.0
+    with nogil:
+        for i in xrange(1 + id * sliceSize, (id + 1) * sliceSize):
+            x = (i - 0.5) * delta
+            sum += 1.0 / (1.0 + x * x)
+    return sum
--- Python/processSlice_cython_py3.pyx
+++ Python/processSlice_cython_py3.pyx
@@ -3,9 +3,10 @@
 #  Copyright © 2010–2013 Russel Winder
 
 def processSlice(int id, int sliceSize, double delta):
-  cdef int i = 0
-  cdef double sum = 0.0
-  for i in range(1 + id * sliceSize, (id + 1) * sliceSize):
-    x = (i - 0.5) * delta
-    sum += 1.0 / (1.0 + x * x)
-  return sum
+    cdef int i = 0
+    cdef double sum = 0.0
+    with nogil:
+        for i in range(1 + id * sliceSize, (id + 1) * sliceSize):
+            x = (i - 0.5) * delta
+            sum += 1.0 / (1.0 + x * x)
+    return sum
