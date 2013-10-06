84c1d209afcd37010ab6b1d1e9debd5bb97e8cd4
Russel Winder <russel@winder.org.uk>
2013-10-06 12:05:10 +0100

    Tidy up the syntax of the Kotlin examples.

--- Kotlin/src/pi_sequential_loop.kt
+++ Kotlin/src/pi_sequential_loop.kt
@@ -1,15 +1,17 @@
-package uk.org.winder.pi_quadrature
+package uk.org.winder.pi_quadrature.pi_sequential_loop
+
+import uk.org.winder.pi_quadrature.out
 
 fun main(args:Array<String>) {
-  val n = 1000000000;
-  val delta = 1.0 / n;
-  val startTimeNanos = System.nanoTime();
-  var sum = 0.0;
+  val n = 1000000000
+  val delta = 1.0 / n
+  val startTimeNanos = System.nanoTime()
+  var sum = 0.0
   for (i in 1..n) {
-     val x = (i - 0.5) * delta;
-     sum += 1.0 / (1.0 + x * x);
-   }
-   val pi = 4.0 * delta * sum;
-   val elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
-   out("pi_sequential_loop", pi, n, elapseTime);
+    val x = (i - 0.5) * delta
+    sum += 1.0 / (1.0 + x * x)
+  }
+  val pi = 4.0 * delta * sum
+  val elapseTime = (System.nanoTime() - startTimeNanos) / 1e9
+  out("pi_sequential_loop", pi, n, elapseTime)
 }
