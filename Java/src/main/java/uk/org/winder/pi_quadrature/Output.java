ac33366d46b090c8afac26d85fb0ee42b8731f24
Russel Winder <russel@winder.org.uk>
2013-10-10 15:52:25 +0100

    Make the Java output consisten with that of other language versions.

--- Java/src/main/java/uk/org/winder/pi_quadrature/Output.java
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Output.java
@@ -9,16 +9,15 @@ package uk.org.winder.pi_quadrature;
 public class Output {
 
   public static void out(final String prefix, final Double pi, final Integer n, final Double elapseTime) {
-    System.out.println("================ " + prefix);
+    System.out.println("====================== " + prefix);
     System.out.println("\tπ = " + pi);
     System.out.println("\titeration count = " + n);
     System.out.println("\telapse time = " + elapseTime);
   }
 
   public static void out(final String prefix, final Double pi, final Integer n, final Double elapseTime, final Integer numberOfTasks) {
-    out(prefix, pi, n, elapseTime);
+    out(prefix + ": task count = " + numberOfTasks.toString(), pi, n, elapseTime);
     System.out.println("\tprocessor count = " + Runtime.getRuntime().availableProcessors());
-    System.out.println("\tnumber of tasks = " + numberOfTasks);
   }
 
 }
