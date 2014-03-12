21574229777928db21295430d523075917bbc15b
Russel Winder <russel@winder.org.uk>
2014-03-12 21:18:32 +0000

    Distinguish batched and unbatched paralle Java 8 versions.

---
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Pi_ParallelStream_Java8_NotBatched.java
@@ -0,0 +1,30 @@
+/*
+ *  Calculation of π using quadrature realized with an approached based on Java 8 streams.
+ *
+ *  Copyright © 2013  Russel Winder
+ */
+
+package uk.org.winder.pi_quadrature;
+
+import java.util.stream.IntStream;
+
+public class Pi_ParallelStream_Java8_Alt {
+  private static void execute(final int numberOfTasks) {
+    final int n = 1000000000;
+    final double delta = 1.0 / n;
+    final long startTimeNanos = System.nanoTime();
+    final int sliceSize = n / numberOfTasks;
+    final double pi = 4.0 * delta * IntStream.range(0, n).parallel().mapToDouble(i -> {
+        final double x = (i - 0.5) * delta;
+        return 1.0 / (1.0 + x * x);
+      }).sum();
+    final double elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
+    Output.out("Pi_ParallelStream_Java8", pi, n, elapseTime, numberOfTasks);
+  }
+  public static void main(final String[] args) {
+    Pi_ParallelStream_Java8_Alt.execute(1);
+    Pi_ParallelStream_Java8_Alt.execute(2);
+    Pi_ParallelStream_Java8_Alt.execute(8);
+    Pi_ParallelStream_Java8_Alt.execute(32);
+  }
+}
