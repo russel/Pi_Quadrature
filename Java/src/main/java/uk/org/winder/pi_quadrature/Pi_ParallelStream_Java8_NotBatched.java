cc0fdd04cec142d5b8d4fc0b3abb8b6bd2d75748
Russel Winder <russel@winder.org.uk>
2014-03-31 15:10:04 +0100

    Remove the multiple executions since there was no difference between them.

--- Java/src/main/java/uk/org/winder/pi_quadrature/Pi_ParallelStream_Java8_NotBatched.java
+++ Java/src/main/java/uk/org/winder/pi_quadrature/Pi_ParallelStream_Java8_NotBatched.java
@@ -9,7 +9,7 @@ package uk.org.winder.pi_quadrature;
 import java.util.stream.IntStream;
 
 public class Pi_ParallelStream_Java8_NotBatched {
-  private static void execute(final int numberOfTasks) {
+  public static void main(final String[] args) {
     final int n = 1000000000;
     final double delta = 1.0 / n;
     final long startTimeNanos = System.nanoTime();
@@ -18,12 +18,6 @@ public class Pi_ParallelStream_Java8_NotBatched {
         return 1.0 / (1.0 + x * x);
       }).sum();
     final double elapseTime = (System.nanoTime() - startTimeNanos) / 1e9;
-    Output.out("Pi_ParallelStream_Java8_NotBatched", pi, n, elapseTime, numberOfTasks);
-  }
-  public static void main(final String[] args) {
-    Pi_ParallelStream_Java8_NotBatched.execute(1);
-    Pi_ParallelStream_Java8_NotBatched.execute(2);
-    Pi_ParallelStream_Java8_NotBatched.execute(8);
-    Pi_ParallelStream_Java8_NotBatched.execute(32);
+    Output.out("Pi_ParallelStream_Java8_NotBatched", pi, n, elapseTime);
   }
 }
