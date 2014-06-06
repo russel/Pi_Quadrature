3e385f1030f98d34cb48a071611361f93b5f7306
Russel Winder <russel@winder.org.uk>
2014-06-06 19:16:50 +0100

    Experiment with enforcement of 'read only' parameters.

--- D/outputFunctions.d
+++ D/outputFunctions.d
@@ -1,21 +1,21 @@
 /*
  *  Output functions for the D realizations of π by quadrature.
  *
- *  Copyright © 2012  Russel Winder
+ *  Copyright © 2012, 2014  Russel Winder
  */
 
 import std.conv;
 import std.parallelism;
 import std.stdio;
 
-void output(string banner, double pi, int n, double elapseTime) {
+void output(in string banner, in double pi, in int n, in double elapseTime) {
   writeln("======================== " ~ banner);
   writefln("\tπ = %.18f", pi);
   writefln("\titeration count = %d", n);
   writefln("\telapse time = %f", elapseTime);
 }
 
-void output(string banner, double pi, int n, double elapseTime, int numberOfTasks) {
+void output(in string banner, in double pi, in int n, in double elapseTime, in int numberOfTasks) {
   output(banner ~ ", task count: " ~ to!string(numberOfTasks), pi, n, elapseTime);
   writefln("\tnumber of processors = %d", totalCPUs);
 }
