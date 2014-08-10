23640e9573ea51bf5f26173e6f7fc79e148b3642
Russel Winder <russel@winder.org.uk>
2014-08-10 14:59:43 +0100

    Add fold implemented variants of sequential algorithm for Rust codes.

---
+++ Rust/pi_sequential_fold.rs
@@ -0,0 +1,24 @@
+/*
+ *  Sequential implementation of π by quadrature using imperative approach.
+ *
+ *  Copyright © 2013, 2014  Russel Winder
+ */
+
+extern crate time;
+extern crate output;
+
+use std::iter::range_inclusive;
+use time::precise_time_s;
+use output::output;
+
+fn main() {
+    let n = 1000000000u;
+    let delta = 1.0 / n as f64;
+    let startTime = precise_time_s();
+    let pi = 4.0 * delta * range_inclusive(1u, n).fold(0.0, |acc, i| {
+        let x = (i as f64 - 0.5) * delta;
+        acc + 1.0 / (1.0 + x * x)
+    });
+    let elapseTime = precise_time_s() - startTime;
+    output("pi_sequential_fold".to_string(), pi, n, elapseTime)
+}
