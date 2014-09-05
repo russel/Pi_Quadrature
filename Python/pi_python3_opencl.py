206bceb5124ba0284711e6ed5240d9261a28ff91
Russel Winder <russel@winder.org.uk>
2014-09-05 16:46:50 +0100

    Changes to the PyOpenCL codes to make them more reasonable.

--- Python/pi_python3_opencl.py
+++ Python/pi_python3_opencl.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
-#  Calculation of π using quadrature.  Using PyOpenCL.
+#  Calculation of π using quadrature. Using PyOpenCL.
 #
-#  Copyright © 2012  Russel Winder
+#  Copyright © 2012, 2014  Russel Winder
 
 from time import time
 
@@ -12,25 +12,18 @@ import numpy
 
 from output import out
 
-def execute(workerCount):
-    n = 1000000000
-    delta = 1.0 / n
-    startTime = time()
-    sliceSize = n // workerCount
-    context = create_some_context()
-    queue = CommandQueue(context)
-    with open('processSlice_opencl.cl', 'r') as f:
-        kernel = Program(context, f.read()).build()
-    results = numpy.array(range(workerCount), dtype=numpy.float64)
-    buffer = Buffer(context, mem_flags.WRITE_ONLY, results.nbytes)
-    kernel.processSlice(queue, results.shape, None, numpy.int32(sliceSize), numpy.float64(delta), buffer)
-    enqueue_read_buffer(queue, buffer, results).wait()
-    pi = 4.0 * delta * numpy.sum(results)
-    elapseTime = time() - startTime
-    out(__file__, pi, n, elapseTime)
-
-if __name__ == '__main__':
-    execute(1)
-    execute(2)
-    execute(8)
-    execute(32)
+n = 1000000000
+delta = 1.0 / n
+startTime = time()
+context = create_some_context()
+queue = CommandQueue(context)
+with open('processSlice_opencl.cl', 'r') as f:
+    kernel = Program(context, f.read()).build()
+# Quadro FX 570 card on Anglides only supports 32-bit operations, hence float not double.
+results = numpy.array(n, dtype=numpy.float32)
+buffer = Buffer(context, mem_flags.WRITE_ONLY, results.nbytes)
+kernel.processSlice(queue, results.shape, None, numpy.int32(n), numpy.float32(delta), buffer)
+enqueue_read_buffer(queue, buffer, results).wait()
+pi = 4.0 * delta * numpy.sum(results)
+elapseTime = time() - startTime
+out(__file__, pi, n, elapseTime)
--- Python/processSlice_opencl.cl
+++ Python/processSlice_opencl.cl
@@ -1,21 +1,16 @@
 // -*- mode:c; -*-
 
 /*
- *  An OpenCL kernel for calculating slices of the π  by Quadrature problem.
+ *  An OpenCL kernel for calculating slices of the π by Quadrature problem.
  *
- *  Copyright © 2012  Russel Winder
+ *  Copyright © 2012, 2014  Russel Winder
  *
  *  Author: Russel Winder <russel@winder.org.uk>
  */
 
-__kernel void processSlice(int sliceSize, double delta, __global double * results) {
-  unsigned int const id = get_global_id(0);
-  int const start = 1 + id * sliceSize;
-  int const end = (id + 1) * sliceSize + 1;
-  double sum = 0.0;
-  for (int i = start; i < end; ++i) {
-    double const x =  (i - 0.5) * delta;
-    sum += 1.0 / (1.0 + x * x);
-  }
-  results[id] = sum;
+// Quadro FX 570 card on Anglides only supports 32-bit operations, hence float not double.
+
+__kernel void processSlice(int const id, float const delta, __global float * results) {
+  float const x =  (id - 0.5) * delta;
+  results[id] = 1.0 / (1.0 + x * x);
 }
