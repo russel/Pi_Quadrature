b551dd344f2b0e2d17cfc611f89dc19152944ca3
Russel Winder <russel@winder.org.uk>
2014-10-16 13:39:03 +0100

    Update Kotlin versions: reduce now works properly and the Java interoperability is significantly improved.

--- Kotlin/src/pi_sequential_reduce.kt
+++ Kotlin/src/pi_sequential_reduce.kt
@@ -9,7 +9,7 @@ package uk.org.winder.pi_quadrature.pi_sequential_reduce
 import uk.org.winder.pi_quadrature.out
 
 fun main(args:Array<String>) {
-  val n = 10000000  //  100 times fewer  due to speed issues.
+  val n = 1000000000
   val delta = 1.0 / n
   val startTimeNanos = System.nanoTime()
   val pi = 4.0 * delta * (1.0..n).reduce({t, i ->
--- Kotlin/src/pi_streams.kt
+++ Kotlin/src/pi_streams.kt
@@ -14,10 +14,10 @@ fun main(args:Array<String>) {
   val n = 1000000000
   val delta = 1.0 / n
   val startTimeNanos = System.nanoTime()
-  val pi = 4.0 * delta * IntStream.range(1, n)!!.parallel()!!.mapToDouble({i ->
+  val pi = 4.0 * delta * IntStream.range(1, n).parallel().mapToDouble({i ->
     val x = (i - 0.5) * delta
     1.0 / (1.0 + x * x)
-  })!!.sum()
+  }).sum()
   val elapseTime = (System.nanoTime() - startTimeNanos) / 1e9
   out("pi_streams", pi, n, elapseTime)
 }
