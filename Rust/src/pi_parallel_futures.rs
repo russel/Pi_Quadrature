8153bed7793b00335e31cf875b129fec67989ec8
Russel Winder <russel@winder.org.uk>
2014-12-19 18:31:54 +0000

    Update for change in Rust language.

--- Rust/src/pi_parallel_futures.rs
+++ Rust/src/pi_parallel_futures.rs
@@ -17,7 +17,7 @@ fn execute(number_of_tasks:uint) {
     let delta = 1.0 / n as f64;
     let start_time = precise_time_s();
     let slice_size = n / number_of_tasks;
-    let mut futures = Vec::from_fn(number_of_tasks, |id| Future::spawn(proc() {
+    let mut futures = Vec::from_fn(number_of_tasks, |id| Future::spawn(move || {
         let mut sum:f64 = 0.0;
         for i in range(1 + id * slice_size, (id + 1) * slice_size) {
             let x = (i as f64 - 0.5) * delta;
