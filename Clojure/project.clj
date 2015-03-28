28a51e2be4542349c70434c74f74ba4748db5a9b
Russel Winder <russel@winder.org.uk>
2015-03-28 17:09:18 +0000

    Update project file and git ignore file for the Eclipse/CCW project generation.

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,8 +1,8 @@
 ;  Calculation of π using quadrature.
 ;
-;  Copyright © 2013, 2014  Russel Winder
+;  Copyright © 2013–2015  Russel Winder
 
-(defproject pi_quadrature "0.0.0-SNAPSHOT"
+(defproject pi_quadrature_clojure "0.0.0-SNAPSHOT"
   :description "A collection of functions to calculate π by quadrature with various realizations."
   :url "https://github.com/russel/Pi_Quadrature"
   :license {:name "GPL v.3"
