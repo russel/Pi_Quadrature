5b3d834d87f7cc01d54452aceada38aeefcf68bb
Russel Winder <russel@winder.org.uk>
2015-04-05 15:30:33 +0100

    Update code for latest Kotlin plugin.

--- Kotlin/src/pi_sequential_map_reduce.kt
+++ Kotlin/src/pi_sequential_map_reduce.kt
@@ -12,10 +12,10 @@ fun main(args:Array<String>) {
   val n = 1000000000
   val delta = 1.0 / n
   val startTimeNanos = System.nanoTime()
-  val pi = 4.0 * delta * (1..n).stream().map({(i):Double ->
+  val pi = 4.0 * delta * (1..n).sequence().map {i ->
     val x = (i - 0.5) * delta
     1.0 / (1.0 + x * x)
-  }).reduce{(t, i) -> t + i}
+  }.reduce{t, i -> t + i}
   val elapseTime = (System.nanoTime() - startTimeNanos) / 1e9
   out("pi_sequential_map_reduce", pi, n, elapseTime)
 }
