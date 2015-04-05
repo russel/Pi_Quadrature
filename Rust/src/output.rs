fe10811f1008a53a2d2e7288f7661da83f67f0f1
Russel Winder <russel@winder.org.uk>
2015-04-05 13:51:40 +0100

    Deal with the removal of the std::os::num_cpus function.

--- Rust/src/output.rs
+++ Rust/src/output.rs
@@ -6,8 +6,11 @@
 
 #![crate_name = "output"]
 
+// std::os::num_cpus has been removed from Rust, but there is an RFC to get it re-instated. For now use the
+// num_cpus package instead.
+extern crate num_cpus;
+
 use std::string::String;
-use std::rt::default_sched_threads;
 
 pub fn output(banner:String, pi:f64, n:u64, elapse_time:f64) {
     println!("======================== {}", banner);
@@ -18,5 +21,6 @@ pub fn output(banner:String, pi:f64, n:u64, elapse_time:f64) {
 
 pub fn output_n(banner:String, pi:f64, n:u64, elapse_time:f64, number_of_tasks:u64) {
     output(format!("{}, task count: {}", banner, number_of_tasks), pi, n, elapse_time);
-    println!("\tnumber of scheduler threads = {}", default_sched_threads());
+    println!("\tnumber of cores = {}", num_cpus::get());
+    //println!("\tnumber of cores = {}", std::os::num_cpus());
 }
