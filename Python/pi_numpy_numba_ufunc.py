f18182f7f6cb4b9ede54690175e22023ac7c2211
Russel Winder <russel@winder.org.uk>
2015-09-19 11:23:04 +0100

    Add new Numpy/Numba variant.

---
+++ Python/pi_numpy_numba_ufunc.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+#  Calculation of π using quadrature. Sequential algorithm using Numba Ufunc on NumPy array.
+#
+#  Copyright © 2008–2013, 2015  Russel Winder
+
+from numpy import arange, double
+
+from numba import vectorize
+
+from output import out
+from time import time
+
+
+@vectorize
+def f(i):
+    x = (i - 0.5) * delta
+    return 1.0 / (1.0 + x * x)
+
+
+if __name__ == '__main__':
+    n = 100000000  # 10 times fewer to avoid MemoryError
+    delta = 1.0 / n
+    startTime = time()
+    pi = 4.0 * delta * f(arange(n, dtype=double)).sum()
+    elapseTime = time() - startTime
+    out(__file__, pi, n, elapseTime)
