9010e0ef1b9b916a69fe3e2407712202a41f04a4
Russel Winder <russel@winder.org.uk>
2015-10-04 12:27:39 +0100

    Add some Haskell foldl versions.

---
+++ Haskell/pi_sequential_foldl_let.hs
@@ -0,0 +1,20 @@
+--  Haskell implementation of π by Quadrature
+--
+--  Copyright © 2015  Russel Winder
+
+module Main where
+
+import Output (out)
+
+f :: Double -> Double -> Int -> Double
+f delta t n =
+  let x = ((fromIntegral n) - 0.5) * delta
+  in t + 1.0 / (1.0 + x * x)
+
+main :: IO()
+main =
+  let
+    n = 100000000 -- 10 times fewer for speed reasons.
+    delta = 1.0 / (fromIntegral n)
+  in
+    out "Sequential Foldl Let" (4.0 * delta * foldl (f delta) 0.0 [1..n]) n
---
+++ Haskell/pi_sequential_foldl_where.hs
@@ -0,0 +1,18 @@
+--  Haskell implementation of π by Quadrature
+--
+--  Copyright © 2015  Russel Winder
+
+module Main where
+
+import Output (out)
+
+f :: Double -> Double -> Int -> Double
+f delta t n = 1.0 / (1.0 + x * x)
+  where
+    x = ((fromIntegral n) - 0.5) * delta
+
+main :: IO()
+main =  out "Sequential Foldl Where" (4.0 * delta * foldl (f delta) 0.0 [1..n]) n
+  where
+    n = 100000000 -- 10 times fewer due to speed.
+    delta = 1.0 / (fromIntegral n)
