a27cb5ddacddfd0c5ded60c1b804052ad968d9fc
Russel Winder <russel@winder.org.uk>
2016-10-30 13:36:20 +0000

    Update the TBB version a bit, and add a new variant on the theme.

---
+++ C++/pi_parallel_tbb_reduce_functional.cpp
@@ -0,0 +1,39 @@
+/*
+ *  A C program to calculate π using quadrature as a TBB functional parallel reduce implemented algorithm.
+ *
+ *  Copyright © 2009–2012, 2013, 2014, 2016  Russel Winder
+ */
+
+#include <functional>
+#include <chrono>
+
+#include "tbb/task_scheduler_init.h"
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_reduce.h"
+#include "tbb/mutex.h"
+
+#include "output.hpp"
+
+int main() {
+  auto const n = 1000000000;
+  auto const delta = 1.0 / n;
+  auto const startTime = std::chrono::steady_clock::now();
+  tbb::task_scheduler_init tbb_initializer;
+  auto const pi = 4.0 * delta * tbb::parallel_reduce(
+																										 tbb::blocked_range<long>(0, n),
+																										 0.0,
+																										 [=](tbb::blocked_range<long> const & range, double value) -> double {
+																											 // TBB does not support range-based for on ranges :-(
+																											 //for (auto && i : range) {
+																											 for (auto i = range.begin(); i != range.end(); ++i) {
+																												 auto const x = (i - 0.5) * delta;
+																												 value += 1.0 / (1.0 + x * x);
+																											 }
+																											 return value;
+																										 },
+																										 std::plus<double>()
+																										 );
+  auto const elapseTime = std::chrono::steady_clock::now() - startTime;
+  out("TBB Parallel Reduce Functional", pi, n, elapseTime, tbb::task_scheduler_init::default_num_threads(), tbb::task_scheduler_init::default_num_threads());
+  return 0;
+}
--- C++/pi_parallel_tbb_reduce_imperative.cpp
+++ C++/pi_parallel_tbb_reduce_imperative.cpp
@@ -1,7 +1,7 @@
 /*
- *  A C program to calculate π using quadrature as a TBB implemented algorithm.
+ *  A C program to calculate π using quadrature as a TBB imperative parallel reduce implemented algorithm.
  *
- *  Copyright © 2009–2012, 2013, 2014  Russel Winder
+ *  Copyright © 2009–2012, 2013, 2014, 2016  Russel Winder
  */
 
 #include <chrono>
@@ -23,9 +23,9 @@ class partialSum {
   partialSum(double const d): delta(d), sum(0.0) {}
   partialSum(partialSum const & x, tbb::split): delta(x.delta), sum(0.0) {}
   void operator()(tbb::blocked_range<long> const & range) {
-		// TBB does not support range-based for :-(
-    //for (auto i = range.begin(); i != std::end(range); ++i) {
-    for (auto i = std::begin(range); i != std::end(range); ++i) {
+		// TBB does not support range-based for on ranges :-(
+    //for (auto && i : range) {
+		for (auto i = range.begin(); i != range.end(); ++i) {
       auto const x = (i - 0.5) * delta;
       sum += 1.0 / (1.0 + x * x);
     }
@@ -43,9 +43,9 @@ int main() {
   auto const startTime = std::chrono::steady_clock::now();
   tbb::task_scheduler_init tbb_initializer;
   partialSum accumulator(delta);
-  tbb::parallel_reduce(tbb::blocked_range<long>(0, n), accumulator, tbb::auto_partitioner());
+  tbb::parallel_reduce(tbb::blocked_range<long>(0, n), accumulator);
   auto const pi = 4.0 * delta * accumulator.getSum();
   auto const elapseTime = std::chrono::steady_clock::now() - startTime;
-  out("TBB Batched", pi, n, elapseTime, tbb::task_scheduler_init::default_num_threads(), tbb::task_scheduler_init::default_num_threads());
+  out("TBB Parallel Reduce Imperative", pi, n, elapseTime, tbb::task_scheduler_init::default_num_threads(), tbb::task_scheduler_init::default_num_threads());
   return 0;
 }
